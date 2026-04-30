d05d1e40dd3b3eaa1ed3e84d58f174433ca929ad
Lauri Nomme <lauri.nomme@gmail.com>
2026-05-01 00:36:01 +0300

    Add commitDocument to data flow commands (matches diagnostics flow)

--- server/src/main/java/tf/locals/idealsp/server/dataflow/DataFlowFromCommand.java
+++ server/src/main/java/tf/locals/idealsp/server/dataflow/DataFlowFromCommand.java
@@ -49,7 +49,16 @@ public class DataFlowFromCommand extends LspCommand<List<DataFlowLocation>> {
 
         var disposable = Disposer.newDisposable();
         try {
-            EditorUtil.withEditor(disposable, ctx.getPsiFile(), pos, editor -> {
+            // Commit PSI document to ensure up-to-date PSI tree (matching diagnostics flow)
+            PsiFile psiFile = ctx.getPsiFile();
+            if (psiFile.getVirtualFile() != null) {
+                var doc = com.intellij.openapi.fileEditor.FileDocumentManager.getInstance().getDocument(psiFile.getVirtualFile());
+                if (doc != null) {
+                    com.intellij.psi.PsiDocumentManager.getInstance(ctx.getProject()).commitDocument(doc);
+                }
+            }
+
+            EditorUtil.withEditor(disposable, psiFile, pos, editor -> {
                 int offset = MiscUtil.positionToOffset(MiscUtil.getDocument(ctx.getPsiFile()), pos);
                 PsiElement element = ctx.getPsiFile().findElementAt(offset);
 
